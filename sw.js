8912c4c810e66ea9bf2d7c413cd45f30a197704e
Durga Prasad <119413312+MrDPrasad@users.noreply.github.com>
2026-03-23 19:31:27 +0530

    Upgrade service worker cache version to 'tord-v3'

--- sw.js
+++ sw.js
@@ -1,15 +1,33 @@
-const CACHE = 'tord-v2';
+const CACHE = 'tord-v3';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
+
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  // Network-first for Firebase/CDN, cache-first for static assets
+  const url = new URL(e.request.url);
+  const isStatic = ASSETS.some(a => url.pathname === a || url.pathname.endsWith(a));
+  if (isStatic) {
+    e.respondWith(
+      caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }))
+    );
+  } else {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  }
 });
